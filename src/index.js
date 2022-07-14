058aa30625e11e7d7fe9552ba17af80ff408d413
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-07-14 09:53:20 +0430

    Add remove action

--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
+import { MyToDo, OneTaskListItem } from './modules/todoObjectClass.js';
 import './style.css';
 
-const lists = [
-  {
-    description: 'Learn JavaScript',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Join Microverse',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Go for a walk',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Take a rest',
-    completed: false,
-    index: 4,
-  },
-];
-
-const listItem = () => {
-  const listContainer = document.querySelector('.listContainer');
-  const unorderedlist = document.createElement('ul');
-  unorderedlist.className = 'listItems';
-  listContainer.appendChild(unorderedlist);
-  for (let i = 0; i < lists.length; i += 1) {
-    const onelist = document.createElement('li');
-    onelist.className = 'onelist';
-    onelist.innerHTML = `
-    <input class="checkbox" type="checkbox"/>
-    <label id="${lists[i].index}" class="label">${lists[i].description} <i class="fa-solid fa-ellipsis-vertical fa-icon"></i></label>`;
-    unorderedlist.appendChild(onelist);
-  }
-  return listContainer;
+const task = new OneTaskListItem();
+const addListItem = document.querySelector('#add');
+addListItem.addEventListener('change', () => {
+  const AddedItem = new OneTaskListItem();
+  AddedItem.addNewToDo(MyToDo.tasks, addListItem.value);
+});
+const clearSelectedTasks = document.querySelector('.clear');
+clearSelectedTasks.addEventListener('click', () => {
+  MyToDo.tasks = MyToDo.tasks.filter((task) => task.completed === false);
+  localStorage.setItem('todotasks', JSON.stringify(MyToDo.tasks));
+  task.displayToDoList();
+});
+window.onload = () => {
+  task.displayToDoList();
 };
-listItem();
